014bdb5927900181058aecdde6efaef6232c5748
krutidadriwal <krutidadriwal1834@gmail.com>
2026-06-20 19:54:40 +0530

    vercel change-5

--- api/barcode/products.ts
+++ api/barcode/products.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { SupabaseProductRepository } from '../_lib/SupabaseProductRepository';
+import { SupabaseProductRepository } from '../_lib/SupabaseProductRepository.ts';
 
 const repository = new SupabaseProductRepository();
 
